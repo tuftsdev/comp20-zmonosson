7da1a7fec4fe6ad41cb6061fb12d82f33c5e2bf5
Zoe Monosson <zoemonosson@wr-130-64-222-74.medford.tufts.edu>
2015-03-04 16:20:57 -0500

    just google appears

--- mspacman/game.js
+++ mspacman/game.js
@@ -3,7 +3,7 @@ function init(){
     var ctx = canvas.getContext('2d');
     var img = new Image();
     img.onload = function(){
-		ctx.drawImage(img, 0, 0);
+	ctx.drawImage(img, 320, 0, 465, 140,0, 0,465,140);
 	};
     img.src = 'pacman10-hp-sprite.png'; 
 }

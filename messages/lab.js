014fec5f11f5e3df0b4a7d9f66939606a85c32c9
Zoe Monosson <zoemonosson@resnet150-57.medford.tufts.edu>
2015-03-07 17:12:47 -0500

    first attempt

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,20 @@
 // Your JavaScript goes here...
+
+function parse(){
+    var request;
+    request = new XMLHttpRequst();
+    
+    request.readyState();
+
+    request.send();
+
+    if (request.readyState() == 4){
+	messagesDiv = document.getElementById( "messages");
+	converted = JSON.parese(request.responseText);   
+	for(i= 0; i <converted.length; i++){
+	    messagesDiv.innerHTML += "<p>" + converted[i]["content"];
+	    }
+    }
+
+
+}
